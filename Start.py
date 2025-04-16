50a85deafa766e5841268650c2b28ea20365b807
simona-cyber <simona.flachsmann@gmail.com>
2025-04-16 09:29:57 +0200

    login

--- Start.py
+++ Start.py
@@ -2,6 +2,21 @@ import pandas as pd
 from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
+# Initialisierung des Data Managers (hier mit Verbindung zu SwitchDrive)
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="Albumona") 
+
+# initialize the login manager
+login_manager = LoginManager(data_manager)
+login_manager.login_register()  # open login/register page
+
+# Laden der Daten
+data_manager.load_user_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
+    )
+
 import streamlit as st
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
