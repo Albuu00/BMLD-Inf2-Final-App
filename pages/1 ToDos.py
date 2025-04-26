1d7a53ed7e0e222508690e4e3daf76cfdbc47cc1
Albulena Ibishi <ibishalb@students.zhaw.ch>
2025-04-26 10:22:42 +0200

    back to normal

--- pages/1 ToDos.py
+++ pages/1 ToDos.py
@@ -93,21 +93,10 @@ for i, todo in enumerate(st.session_state.todos):
     col1, col2, col3 = st.columns([0.1, 0.8, 0.1])
     with col1:
         # Checkbox zum Abhaken
-        #checked = st.checkbox("", value=todo["completed"], key=f"todo_{i}")
-        #if checked != todo["completed"]:
-            #toggle_task(i)
-        # Checkbox oder grüner Haken anzeigen
-        if todo["completed"]:
-            # Grüner Haken anzeigen, wenn die Aufgabe erledigt ist
-            st.markdown(
-                f"<span style='color: green; font-size: 20px;'>✔</span>",
-                unsafe_allow_html=True,
-            )
-        else:
-            # Checkbox anzeigen, wenn die Aufgabe nicht erledigt ist
-            if st.checkbox("", value=False, key=f"todo_{i}"):
-                # Status der Aufgabe direkt aktualisieren
-                st.session_state.todos[i]["completed"] = True
+        checked = st.checkbox("", value=todo["completed"], key=f"todo_{i}")
+        if checked != todo["completed"]:
+            toggle_task(i)
+
     with col2:
         # Aufgabe anzeigen (grau, wenn abgehakt)
         if todo["completed"]:
